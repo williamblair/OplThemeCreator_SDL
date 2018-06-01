1aa6e92934bee8b78f3167e8a5a6b11d457e7ef6
williamblair <blairw@sunyit.edu>
2018-06-01 01:26:28 -0400

    formatted documents

--- src/Element.cpp
+++ src/Element.cpp
@@ -1,21 +1,21 @@
 #include "Element.hpp"
 
-Element::Element(void){m_hasImage = false;}
-Element::~Element(void){}
+Element::Element(void) { m_hasImage = false; }
+Element::~Element(void) {}
 
 bool Element::parse(const std::string line)
 {
     std::stringstream ss(line);
-    
+
     /* Make sure we've got a valid string */
-    if(line.back() != ':') {
+    if (line.back() != ':') {
         printf("Element: invalid line on parse: %s\n", line.c_str());
         return false;
     }
-    
+
     /* Get the name and eleminate the ':' at the end */
     std::getline(ss, m_name, ':');
-    
+
     return true;
 }
 
@@ -27,7 +27,7 @@ bool Element::addSetting(Setting setting)
 
 bool Element::addImage(const std::string &imageStr)
 {
-    if(!m_sprite.loadImage(imageStr.c_str())) {
+    if (!m_sprite.loadImage(imageStr.c_str())) {
         return false;
     }
     m_hasImage = true;
@@ -37,7 +37,7 @@ bool Element::addImage(const std::string &imageStr)
 void Element::print(void)
 {
     printf("Element Name: %s:\n", m_name.c_str());
-    for(Setting s : m_settings)
+    for (Setting s : m_settings)
     {
         printf("    ");
         s.print();
@@ -56,25 +56,25 @@ std::string Element::getName(void)
 
 bool Element::draw(SDL_Surface *s)
 {
-    if(m_hasImage) {
+    if (m_hasImage) {
         m_sprite.draw(s);
     }
-    
+
     return true;
 }
 
 bool Element::setPos(int x, int y)
 {
-	m_sprite.setX(x);
-	m_sprite.setY(y);
+    m_sprite.setX(x);
+    m_sprite.setY(y);
 
-	return true;
+    return true;
 }
 
 bool Element::setPosCentered(int x, int y)
 {
-	m_sprite.setX(x - m_sprite.getW() / 2);
-	m_sprite.setY(y - m_sprite.getH() / 2);
+    m_sprite.setX(x - m_sprite.getW() / 2);
+    m_sprite.setY(y - m_sprite.getH() / 2);
 
-	return true;
+    return true;
 }
